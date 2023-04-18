98146c81f964ee15090ac85cbc4134567e4128a8
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-19 04:28:52 +0700

    update Admin.py

--- modules/Admin.py
+++ modules/Admin.py
@@ -36,7 +36,7 @@ from telethon.tl.functions.messages import GetFullChatRequest, SetHistoryTTLRequ
 from telethon.tl.types import InputMessagesFilterPinned
 from telethon.utils import get_display_name
 
-from Ayra.dB import DEVLIST
+from Ayra.dB import DEVS
 from Ayra.fns.admins import ban_time
 
 from . import (
