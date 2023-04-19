b69c71c48ceb5c153fcbbf6484b1d8331aab73a8
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-19 22:19:36 +0700

    update configs.py

--- Ayra/configs.py
+++ Ayra/configs.py
@@ -21,7 +21,7 @@ except ImportError:
 class Var:
     # mandatory
     API_ID = (
-        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=6, cast=int)
+        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=5, cast=int)
     )
     API_HASH = (
         sys.argv[2]
@@ -29,13 +29,8 @@ class Var:
         else config("API_HASH", default=None)
     )
     SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
-    REDIS_URI = (
-        sys.argv[4]
-        if len(sys.argv) > 4
-        else (config("REDIS_URI", default=None) or config("REDIS_URL", default=None))
-    )
-    REDIS_PASSWORD = (
-        sys.argv[5] if len(sys.argv) > 5 else config("REDIS_PASSWORD", default=None)
+    MONGO_URI = (
+        sys.argv[4] if len(sys.argv) > 4 else config("MONGO_URI", default=None)
     )
     # extras
     BOT_TOKEN = config("BOT_TOKEN", default=None)
