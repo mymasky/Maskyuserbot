1a08d560ad4f06a27551a2bb19191998ae64207d
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-22 02:08:08 +0700

    update system.py

--- modules/system.py
+++ modules/system.py
@@ -5,7 +5,7 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
 """
-✘ **Bantuan Untuk Tag All**
+✘ **Bantuan Untuk System**
 
 ๏ **Perintah:** `usage`
 ◉ **Keterangan:** Dapatkan stats penggunaan.
