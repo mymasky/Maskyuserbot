565ec5476afebec91d1627a9a266667ad3c918a1
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-22 00:22:41 +0700

    update toxic2.py

--- modules/toxic2.py
+++ modules/toxic2.py
@@ -49,7 +49,7 @@ async def _(event):
 # Create by myself @localheart
 
 
-@ayra_cmd(outgoing=True, pattern="(g|G)oblok|blk")
+@ayra_cmd(outgoing=True, pattern="(g|Goblok|blk)")
 async def _(event):
     xx = await event.eor("**WOYY GOBLOK!!**")
     sleep(1)
@@ -120,7 +120,7 @@ async def _(event):
     await event.eor("`Tapi Bo'ong Hiyahiyahiya`")
 # Create by myself @localheart
 
-@ayra_cmd(outgoing=True, pattern="(k|K)ont|ntl")
+@ayra_cmd(outgoing=True, pattern="(k|Kont|ntl)")
 async def _(event):
     xx = await event.eor("**KONTOLL**")
     sleep(1.5)
