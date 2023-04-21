d9790540bdc4e555dda11748f834cd8ac07d09f7
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-22 01:25:26 +0700

    update webshot.py

--- modules/webshot.py
+++ modules/webshot.py
@@ -49,7 +49,7 @@ from . import humanbytes as hb
 from . import inline_mention, is_url_ok, mediainfo, ayra_cmd
 
 
-@ayra_cmd(pattern=r"^[wW][ebshot]")
+@ayra_cmd(pattern=r"^(Webshot|Ss)(webshot|ss)")
 async def webss(event):
     xx = await event.eor(get_string("com_1"))
     xurl = event.pattern_match.group(1).strip()
