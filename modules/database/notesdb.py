b36a0da1f3a14b49dc4422306d7341f310d2e925
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 03:43:51 +0700

    update notesdb.py

--- modules/database/notesdb.py
+++ modules/database/notesdb.py
@@ -43,4 +43,4 @@ def get_notes(user, word):
 def list_note(user):
     ok = get_stuff()
     if ok.get(int(user)):
-        return "".join(f"**├ {z}**\n" for z in ok[user])
+        return "".join(f"**๏ {z}**\n" for z in ok[user])
