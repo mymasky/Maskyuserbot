d343dff8d374a8610c48d31e0db5be7ffa11a850
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-22 23:08:52 +0700

    update bot.py

--- modules/bot.py
+++ modules/bot.py
@@ -241,7 +241,7 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-@ayra_cmd(pattern="(Ping|ping)")
+@ayra_cmd(pattern="ping")
 @register(incoming=True, from_users=DEVS, pattern=r"^Cping$")
 async def _(ping):
     uptime = await get_readable_time((time.time() - StartTime))
