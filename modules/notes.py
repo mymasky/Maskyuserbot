8bc4023e07a0ab331dab1a6ca61c84e2f53a02a9
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 03:03:44 +0700

    update notes.py

--- modules/notes.py
+++ modules/notes.py
@@ -29,7 +29,8 @@ from . import *
 async def an(e):
     wrd = (e.pattern_match.group(1).strip()).lower()
     wt = await e.get_reply_message()
-    chat = int(udB.get_key("LOG_CHANNEL"))
+    bahan = int(udB.get_key("LOG_CHANNEL"))
+    user = e.sender_id
     if not (wt and wrd):
         return await e.eor(get_string("notes_1"), time=5)
     if "#" in wrd:
@@ -55,49 +56,44 @@ async def an(e):
             txt = wt.text
             if not btn:
                 txt, btn = get_msg_button(wt.text)
-            add_note(chat, wrd, txt, m, btn)
+            add_note(user, wrd, txt, m, btn)
         else:
-            add_note(chat, wrd, None, m, btn)
+            add_note(user, wrd, None, m, btn)
     else:
         txt = wt.text
         if not btn:
             txt, btn = get_msg_button(wt.text)
-        await e.client.send_message(chat,
-            f"#NOTE\nKEYWORD: {wrd}\n\nPesan berikut disimpan sebagai data balasan catatan untuk obrolan, mohon jangan dihapus !!",
-            reply_to=wt.id
-        )
-        add_note(chat, wrd, txt, None, btn, reply_to=wt.id)
+        add_note(user, wrd, txt, None, btn)
     await e.eor(get_string("notes_2").format(wrd))
     ayra_bot.add_handler(notes, events.NewMessage())
-
 
 
 @ayra_cmd(pattern="rnote( (.*)|$)")
 async def rn(e):
     wrd = (e.pattern_match.group(1).strip()).lower()
-    chat = int(udB.get_key("LOG_CHANNEL"))
+    user = e.sender_id
     if not wrd:
         return await e.eor(get_string("notes_3"), time=5)
     if wrd.startswith("#"):
         wrd = wrd.replace("#", "")
-    rem_note(chat, wrd)
+    rem_note(int(user), wrd)
     await e.eor(f"Selesai Catatan: `#{wrd}` Dihapus.")
 
 
-@ayra_cmd(pattern="notes$")
+@ayra_cmd(pattern="notes")
 async def lsnote(e):
-    chat = udB.get_key("LOG_CHANNEL")
-    if x := list_note(e.int(chat)):
+    user = e.sender_id
+    if x := list_note(user):
         sd = "Catatan Ditemukan Dalam Obrolan Ini Adalah\n\n"
         return await e.eor(sd + x)
     await e.eor(get_string("notes_5"))
 
 
 async def notes(e):
-    chat = e.chat_id
+    user = e.sender_id
     xx = [z.replace("#", "") for z in e.text.lower().split() if z.startswith("#")]
     for word in xx:
-        if k := get_notes(word):
+        if k := get_notes(user, word):
             msg = k["msg"]
             media = k["media"]
             if k.get("button"):
