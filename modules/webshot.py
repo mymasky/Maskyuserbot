afca1d61f90100d59f2981f8d2bbf6202eb8ee7f
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 05:24:57 +0700

    update webshot.py

--- modules/webshot.py
+++ modules/webshot.py
@@ -14,27 +14,21 @@
 import asyncio
 from . import *
 
-@ayra_cmd(pattern="([Ww][e][b][s][h][o][t])( (.*)|$)")
+@ayra_cmd(pattern="webshot(?:\s+(.*))?")
 async def webshot(e):
     await e.eor("`Processing...`")
     try:
-        user_link = e.pattern_match.group(1).strip()
-        try:
-            full_link = f"https://webshot.deam.io/{user_link}/?width=1920&height=1080?delay=2000?type=png"
-            await e.client.send_file(
-                e.chat_id,
-                full_link,
-                caption=f"**Tangkapan layar halaman** {user_link}",
-            )
-        except Exception as dontload:
-            await e.eor(f"Error! `{dontload}`\nMencoba lagi membuat tangkapan layar...")
-            full_link = f"https://mini.s-shot.ru/1920x1080/JPEG/1024/Z100/?{user_link}"
-            await e.client.send_file(
-                e.chat_id,
-                full_link,
-                caption=f"**Tangkapan layar halaman** `{user_link}`",
-            )
-    except Exception as error:
-        await e.eor(
-            e.chat_id, f"**Ada yang salah\nLog:`{error}`...**"
+        user_link = e.pattern_match.group(2)
+        if not user_link:
+            await e.eor("`Masukkan URL situs web yang ingin diambil tangkapan layarnya.`")
+            return
+        full_link = f"https://webshot.deam.io/{user_link}/?width=1920&height=1080&delay=2000&type=png"
+        await e.client.send_file(
+            e.chat_id,
+            full_link,
+            caption=f"**Tangkapan layar halaman** {user_link}",
+            force_document=True,
+            supports_streaming=False
         )
+    except Exception as error:
+        await e.eor(f"**Terjadi kesalahan:** `{error}`")
