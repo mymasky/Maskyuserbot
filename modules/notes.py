dc891d614006c6b7594fb6e63655b08923c00e6e
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 04:03:56 +0700

    update notes.py

--- modules/notes.py
+++ modules/notes.py
@@ -95,8 +95,8 @@ async def lsnote(e):
 @ayra_cmd(pattern="[Gg][e][t]( (.*)|$)")
 async def notes(e):
     user = e.sender_id
-    for word:
-        if k := get_notes(user, word):
+    wrd = (e.pattern_match.group(1).strip()).lower()
+    if k := get_notes(user, word):
             msg = k["msg"]
             media = k["media"]
             if k.get("button"):
