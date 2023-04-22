b41c413c45066f00a0dcd3ae46937af0eaa96630
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 05:40:30 +0700

    update webshot.py

--- modules/webshot.py
+++ modules/webshot.py
@@ -16,13 +16,14 @@ from . import *
 
 @ayra_cmd(pattern="webshot(?:\s+(.*))?")
 async def webshot(e):
-    await e.eor("`Processing...`")
+    ajg = await e.eor("`Processing...`")
     try:
         user_link = e.pattern_match.group(1).strip()
         if not user_link:
             await e.eor("`Masukkan URL situs web yang ingin diambil tangkapan layarnya.`")
             return
         full_link = f"https://mini.s-shot.ru/1920x1080/JPEG/1024/Z100/?{user_link}"
+        await ajg.delete()
         await e.client.send_file(
             e.chat_id,
             full_link,
