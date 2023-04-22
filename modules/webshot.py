be837d3c24df4473e6f45ed0c57051e48d376d46
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 05:38:00 +0700

    update webshot.py

--- modules/webshot.py
+++ modules/webshot.py
@@ -22,7 +22,7 @@ async def webshot(e):
         if not user_link:
             await e.eor("`Masukkan URL situs web yang ingin diambil tangkapan layarnya.`")
             return
-        full_link = f"https://webshot.deam.io/{user_link}/?width=1920&height=1080&delay=2000&type=png"
+        full_link = f"https://mini.s-shot.ru/1920x1080/JPEG/1024/Z100/?{user_link}"
         await e.client.send_file(
             e.chat_id,
             full_link,
