8269291e1bc8599004952dad1612c60af237317b
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-22 19:35:14 +0700

    update vctools.py

--- modules/vctools.py
+++ modules/vctools.py
@@ -57,14 +57,14 @@ async def start_voice(c):
     creator = chat.creator
 
     if not admin and not creator:
-        await eod(xnxx, get_string("stvc_1").format(me.first_name))
+        await eor(xnxx, get_string("stvc_1").format(me.first_name))
         return
     try:
         Nan = Player(c.chat_id)
-        await Nan make_vc_active()
+        await Nan.make_vc_active()
         await xnxx.edit(get_string("stvc_2"))
     except Exception as ex:
-        await eod(xnxx, get_string("error_1").format(e))
+        await eor(xnxx, get_string("error_1").format(e))
 
 
 @kynan_cmd(pattern="stopvc$", group_only=True)
@@ -77,13 +77,13 @@ async def stop_voice(c):
     creator = chat.creator
 
     if not admin and not creator:
-        await eod(yins, get_string("stvc_1").format(me.first_name))
+        await eor(jing, get_string("stvc_1").format(me.first_name))
         return
     try:
         await c.client(stopvc(await get_call(c)))
         await yins.edit(get_string("stvc_3"))
     except Exception as ex:
-        await eod(jing, get_string("error_1").format(ex))
+        await eor(jing, get_string("error_1").format(ex))
 
 @ayra_cmd(pattern="(v|V)ctitle( (.*)|$)")
 async def _(event):
