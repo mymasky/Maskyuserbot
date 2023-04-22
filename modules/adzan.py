2f0dbcd1a1b2278c1ffab6ed50c65f6edaeb268e
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 04:25:24 +0700

    Create adzan.py

---
+++ modules/adzan.py
@@ -0,0 +1,34 @@
+"""
+✘ **Bantuan Untuk Webshot**
+
+๏ **Perintah:** `adzan` <nama kota>
+◉ **Keterangan:** Dapatkan jadwal adzan.
+"""
+import json
+
+import requests
+from . import *
+
+
+@ayra_cmd(pattern="(A|a)dzan(?:\\s|$)([\\s\\S]*)")
+async def get_adzan(adzan):
+    LOKASI = adzan.pattern_match.group(1)
+    if not LOKASI:
+        await e.eor("<i>Silahkan Masukkan Nama Kota Anda</i>")
+        return True
+    url = f"http://muslimsalat.com/{LOKASI}.json?key=bd099c5825cbedb9aa934e255a81a5fc"
+    request = requests.get(url)
+    if request.status_code != 200:
+        return await eor(f"**Tidak Dapat Menemukan Kota** `{LOKASI}`", time=120)
+    result = json.loads(request.text)
+    catresult = f"<b>Jadwal Shalat Hari Ini:</b>\
+            \n<b>📆 Tanggal </b><code>{result['items'][0]['date_for']}</code>\
+            \n<b>📍 Kota</b> <code>{result['query']}</code> | <code>{result['country']}</code>\
+            \n\n<b>Terbit  : </b><code>{result['items'][0]['shurooq']}</code>\
+            \n<b>Subuh : </b><code>{result['items'][0]['fajr']}</code>\
+            \n<b>Zuhur  : </b><code>{result['items'][0]['dhuhr']}</code>\
+            \n<b>Ashar  : </b><code>{result['items'][0]['asr']}</code>\
+            \n<b>Maghrib : </b><code>{result['items'][0]['maghrib']}</code>\
+            \n<b>Isya : </b><code>{result['items'][0]['isha']}</code>\
+    "
+    await eor(adzan, catresult, "html")
