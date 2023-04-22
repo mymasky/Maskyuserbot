10e2c733c9540dc3951880a941a20822676e7ceb
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-22 22:56:09 +0700

    update nulis.py

--- modules/nulis.py
+++ modules/nulis.py
@@ -15,14 +15,18 @@ from telethon.errors import ChatSendMediaForbiddenError
 import requests
 from . import *
 
+
 @ayra_cmd(pattern=r"(N|n)ulis( (.*)|$)")
 async def handwrite(event):
-    message = event.message
-    text = message.reply_to_message.text if message.reply_to_message else event.pattern_match.group(1)
-    m = await message.reply("`Processing...`")
+    reply_msg = await event.get_reply_message()
+    if reply_msg:
+        text = reply_msg.text
+    else:
+        text = event.pattern_match.group(3)
+    m = await event.reply("`Processing...`")
     req = requests.get(f"https://api.sdbots.tk/write?text={text}").url
     try:
-        await event.send_file(message.chat_id, req, caption=f"Ditulis Oleh: {OWNER_NAME}")
+        await event.client.send_file(event.chat.id, req, caption=f"Ditulis Oleh: {OWNER_NAME}")
     except ChatSendMediaForbiddenError:
         await m.edit("Dilarang mengirim media digrup ini 😥!")
         return
