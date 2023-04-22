9bb2c57bde754eae937865da85a348d01c32a16a
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 05:29:01 +0700

    update webshot.py

--- modules/webshot.py
+++ modules/webshot.py
@@ -18,7 +18,7 @@ from . import *
 async def webshot(e):
     await e.eor("`Processing...`")
     try:
-        user_link = e.pattern_match.group(2)
+        user_link = e.pattern_match.group(1).strip()
         if not user_link:
             await e.eor("`Masukkan URL situs web yang ingin diambil tangkapan layarnya.`")
             return
