c60273d1f107438540c9502f7feacff485089e20
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 10:41:43 +0700

    update chatbot.py

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -23,11 +23,14 @@ from .database.ai import *
 
 @ayra_cmd(pattern="ai( (.*)|$)")
 async def openai(event):
+"""
     OPENAI_API = udB.get_key("OPENAI_API")
     if not OPENAI_API:
         return await event.eor(
             "Ambil OPENAI_API Anda [Disini](https://platform.openai.com/account/api-keys) kemudian ketik {HNDLR}setdb OPENAI_API <api_key>.",
         )
+"""
+    OPENAI_API = "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"
     question = await event.get_reply_message()
     if question:
         text = question.text
@@ -44,7 +47,7 @@ async def openai(event):
         "max_tokens": 500,
         "temperature": 0,
     }
-    msg = await event.eor("`Processing..")
+    msg = await event.eor("`Processing...`")
     try:
         response = (await requests.post("https://api.openai.com/v1/completions", headers=headers, json=json_data)).json()
         await msg.edit(response["choices"][0]["text"])
