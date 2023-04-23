1f217c56b91c668b80c90b3a76afb7340dbed4fa
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 17:59:56 +0700

    update ai.py

--- modules/database/ai.py
+++ modules/database/ai.py
@@ -1,63 +1,24 @@
 
-from asyncio import gather
-
-import httpx
-from aiohttp import ClientSession
+import openai
 
 
-# Aiohttp Async Client
-session = ClientSession()
+class OpenAi:
+    def text(self, question):
+        OPENAI_API = "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"
+        openai.api_key = OPENAI_API
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"Q: {question}\nA:",
+            temperature=0,
+            max_tokens=500,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+        )
+        return response.choices[0].text
 
-# HTTPx Async Client
-http = httpx.AsyncClient(
-    http2=True,
-    timeout=httpx.Timeout(40),
-)
+    def photo(self, question):
+        openai.api_key = OPENAI_API
+        response = openai.Image.create(prompt=question, n=1, size="1024x")
+        return response["data"][0]["url"]
 
-
-
-async def get(url: str, *args, **kwargs):
-    async with session.get(url, *args, **kwargs) as resp:
-        try:
-            data = await resp.json()
-        except Exception:
-            data = await resp.text()
-    return data
-
-
-async def head(url: str, *args, **kwargs):
-    async with session.head(url, *args, **kwargs) as resp:
-        try:
-            data = await resp.json()
-        except Exception:
-            data = await resp.text()
-    return data
-
-
-async def post(url: str, *args, **kwargs):
-    async with session.post(url, *args, **kwargs) as resp:
-        try:
-            data = await resp.json()
-        except Exception:
-            data = await resp.text()
-    return data
-
-
-async def multiget(url: str, times: int, *args, **kwargs):
-    return await gather(*[get(url, *args, **kwargs) for _ in range(times)])
-
-
-async def multihead(url: str, times: int, *args, **kwargs):
-    return await gather(*[head(url, *args, **kwargs) for _ in range(times)])
-
-
-async def multipost(url: str, times: int, *args, **kwargs):
-    return await gather(*[post(url, *args, **kwargs) for _ in range(times)])
-
-
-async def resp_get(url: str, *args, **kwargs):
-    return await session.get(url, *args, **kwargs)
-
-
-async def resp_post(url: str, *args, **kwargs):
-    return await session.post(url, *args, **kwargs)
