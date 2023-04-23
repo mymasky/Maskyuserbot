c3cad4d0a289f20a75a6b885b0967971d54e2455
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 18:01:17 +0700

    update chatbot.py

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -10,36 +10,10 @@
 ๏ **Perintah:** `ai` <balas pesan/berikan pertanyaan>
 ◉ **Keterangan:** Sangat berguna untuk kebutuhan.
 """
-import io
-from io import *
-import os
-import requests
-import openai
-import shutil
-from asyncio import gather
+
 from telethon.errors import MessageNotModifiedError
-from . import ayra_cmd, udB
-
-
-class OpenAi:
-    def text(self, question):
-        openai.api_key = udB.get_key("OPENAI_API")
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=f"Q: {question}\nA:",
-            temperature=0,
-            max_tokens=500,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0,
-        )
-        return response.choices[0].text
-
-    def photo(self, question):
-        openai.api_key = udB.get_key("OPENAI_API")
-        response = openai.Image.create(prompt=question, n=1, size="1024x")
-        return response["data"][0]["url"]
-        
+from . import ayra_cmd, udB, eor
+from .database.ai import OpenAi
 
 @ayra_cmd(pattern="ai( (.*)|$)")
 async def openai(event):
@@ -50,9 +24,9 @@ async def openai(event):
     msg = await event.eor("`Processing...`")
     try:
         response = OpenAi().text(question)
-        await msg.edit(f"**Q:** {question}\n\n**A:** {response}")
+        await msg.eor(f"**Q:** {question}\n\n**A:** {response}")
     except Exception as e:
-        await msg.edit(f"**Q:** {question}\n\n**A:** `Error: {e}`")
+        await msg.eor(f"**Q:** {question}\n\n**A:** `Error: {e}`")
 
 
 @ayra_cmd(pattern="img( (.*)|$)")
