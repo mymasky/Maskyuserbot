597a27f576237a3e56642f691eb0e4677a0657ff
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-23 09:54:19 +0700

    Create toanime.py

---
+++ modules/toanime.py
@@ -0,0 +1,44 @@
+# Ayra - UserBot
+# Copyright (C) 2021-2022 senpai80
+#
+# This file is a part of < https://github.com/senpai80/Ayra/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
+"""
+✘ **Bantuan Untuk To Anime**
+
+๏ **Perintah:** `toanime` <balas foto>
+◉ **Keterangan:** Ubah gambar menjadi anime.
+
+◉ **Note:** Wajah harus terdeteksi.
+"""
+
+
+import asyncio
+import time
+from telethon.tl.types import *
+from telethon.tl.functions.messages import DeleteHistory
+
+
+@ayra_cmd(pattern="toanime ")
+async def convert_image(event):
+    if not event.reply_to_msg_id:
+        return await event.eor("**Mohon Balas Ke Foto**")
+    bot = "@qq_neural_anime_bot"
+    if event.reply_to_msg_id:
+        cot = await event.respond("**Processing...**")
+        await event.client(UnblockRequest(bot))
+        ba = await event.client.forward_to(bot, event.reply_to_msg_id, event.chat_id)
+        await asyncio.sleep(30)
+        await ba.delete()
+        await cot.delete()
+        get_photo = []
+        async for Toanime in client.iter_messages(bot, filter=InputMessagesFilterPhotos):
+            get_photo.append(InputMediaPhoto(Toanime.photo))
+        await event.client.send_file(
+            event.chat_id,
+            get_photo,
+            reply_to=event.message.id,
+        )
+#        user_info = await event.client.get_entity(bot)
+#        await event.client(functions.messages.DeleteHistoryRequest(peer=user_info, max_id=0, just_clear=False, revoke=True))
