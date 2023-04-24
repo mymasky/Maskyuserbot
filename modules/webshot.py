7b8889ce7f3f237467a0f7e58e7c64072b83347e
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-25 01:03:42 +0700

    update webshot.py

--- modules/webshot.py
+++ modules/webshot.py
@@ -36,7 +36,7 @@ async def f2i(e):
     if not html:
         return await eod(e, "`Berikan teks atau balas ke file`")
     html = html.replace("\n", "<br>")
-    shot = WebShot(quality=85)
+    shot = WebShot()
     css = "body {background: white;} p {color: red;}"
     pic = await shot.create_pic_async(html=html, css=css)
     await e.reply(file=pic, force_document=True)
