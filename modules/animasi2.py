bcb67c8bf6d3f8b63a945f470e63a8eeb3ff3c02
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-25 04:51:24 +0700

    update animasi2.py

--- modules/animasi2.py
+++ modules/animasi2.py
@@ -78,7 +78,7 @@ async def _(event):
                   "`\n💔<\\  **Terimakasih**`")
 
 
-@register(pattern=r"\.$(.*)", outgoing=True)
+@register(outgoing=True, pattern=r"^ceritacinta(?: |$)(.*)")
 async def _(event):
 
     if event.fwd_from:
@@ -118,7 +118,7 @@ async def _(event):
             await event.edit(animation_chars[i % 103])
 
 
-@register(pattern=r"\.$(.*)", outgoing=True)
+@register(outgoing=True, pattern=r"^canda(?: |$)(.*)")
 async def _(event):
 
     if event.fwd_from:
@@ -161,7 +161,7 @@ async def _(ayiin):
                      "`\n💖<\\  *Tapi Bo'ong Hiyahiyahiya`")
 
 
-@register(pattern=r"\.$(.*)", outgoing=True)
+@register(outgoing=True, pattern=r"^owner(?: |$)(.*)")
 async def _(event):
 
     if event.fwd_from:
