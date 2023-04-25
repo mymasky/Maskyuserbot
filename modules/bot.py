75d056b05f088c8854afaaea5866462b63977af7
Naya <emiliasayang2@gmail.com>
2023-04-25 07:07:49 +0000

    pylint: auto fixes

--- modules/bot.py
+++ modules/bot.py
@@ -6,7 +6,6 @@
 # <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
 
 
-
 import os
 import sys
 import time
@@ -22,7 +21,6 @@ from telethon.errors.rpcerrorlist import (
 
 from . import *
 
-
 try:
     from git import Repo
 except ImportError:
@@ -31,8 +29,7 @@ except ImportError:
 
 from telethon.utils import resolve_bot_file_id
 
-
-piic ="https://graph.org/file/02f9ca4617cec58377b9d.jpg"
+piic = "https://graph.org/file/02f9ca4617cec58377b9d.jpg"
 
 buttons = [
     [
@@ -76,18 +73,19 @@ absen = [
 async def alive(event):
     text = alive_txt.format(ayra_version, AyraVer, __version__)
     await event.answer(text, alert=True)
-    
+
 
 @register(incoming=True, from_users=DEVS, pattern=r"^Absen$")
 async def kynanabsen(ganteng):
     await ganteng.reply(choice(absen))
 
+
 @register(incoming=True, from_users=DEVS, pattern=r"^Kynan")
 async def naya(naya):
     await naya.reply("**Kynan Punya Nya Naya**🤩")
 
-@ayra_cmd(
-    pattern=r"^[aA][lL][iI][vV][eE](?: |$)(.*)")
+
+@ayra_cmd(pattern=r"^[aA][lL][iI][vV][eE](?: |$)(.*)")
 async def lol(ayra):
     match = ayra.pattern_match.group(1).strip()
     inline = True
@@ -171,6 +169,7 @@ async def lol(ayra):
         buttons=buttons if inline else None,
     )
 
+
 """
 @ayra_cmd(pattern="ping$", incoming=True, from_users=DEVS, chats=[], type=["official", "assistant"])
 async def _(event):
@@ -181,6 +180,7 @@ async def _(event):
     await x.edit(get_string("ping").format(end, uptime))
 """
 
+
 async def get_readable_time(seconds: int) -> str:
     count = 0
     up_time = ""
@@ -189,9 +189,7 @@ async def get_readable_time(seconds: int) -> str:
 
     while count < 4:
         count += 1
-        remainder, result = divmod(
-            seconds, 60) if count < 3 else divmod(
-            seconds, 24)
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
         if seconds == 0 and remainder == 0:
             break
         time_list.append(int(result))
@@ -227,6 +225,7 @@ async def _(ping):
         f"**├ 𝚄𝙿𝚃𝙸𝙼𝙴 :** `{uptime}` \n"
         f"**╰ 𝙾𝚆𝙽𝙴𝚁 :** [{user.first_name}](tg://user?id={user.id})" % (duration)
     )
+
 
 @ayra_cmd(
     pattern="cmds$",
@@ -298,7 +297,7 @@ async def inline_alive(ayra):
     if isinstance(pic, list):
         pic = choice(pic)
     uptime = time_formatter((time.time() - start_time) * 1000)
-    header = udB.get_key("ALIVE_TEXT") or get_string("bot_1")
+    udB.get_key("ALIVE_TEXT") or get_string("bot_1")
     y = Repo().active_branch
     xx = Repo().remotes[0].config_reader.get("url")
     rep = xx.replace(".git", f"/tree/{y}")
