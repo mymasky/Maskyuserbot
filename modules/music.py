36a6c185a3f8d40e4cba20625a4b326c15be3bf6
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-05 00:36:29 +0700

    update music.py

--- modules/music.py
+++ modules/music.py
@@ -60,7 +60,19 @@
 
 from . import *
 from ._music import *
-
+from pytgcalls.exceptions import NotConnectedError
+import re
+import os
+from telethon.tl import functions, types
+from telethon.errors.rpcerrorlist import (
+    ChatSendMediaForbiddenError,
+    ParticipantJoinMissingError,
+    MessageIdInvalidError,
+)
+from Ayra.fns.info import *
+from Ayra.fns.misc import *
+from Ayra.fns.tools import *
+from Ayra.fns.helper import *
 
 @vc_asst("play")
 async def play_music_(event):
