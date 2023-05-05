3708018f7b6132bf5c0c3108df823bc18d75b9e9
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-05 16:55:46 +0700

    update memify.py

--- modules/memify.py
+++ modules/memify.py
@@ -26,21 +26,21 @@ async def handler(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.reply(
+        await event.eor(
             "`Berikan Beberapa Teks Untuk Digambar! Dan Membalas sticker/gambar Contoh : `mmf teks`"
         )
         return
     reply_message = await event.get_reply_message()
     if not reply_message.media:
-        await event.reply("`Balas ke gambar/stiker.`")
+        await event.eor("`Balas ke gambar/stiker.`")
         return
-    file = await bot.download_media(reply_message)
-    msg = await event.reply("`Processing...`")
+    file = await event.client.download_media(reply_message)
+    msg = await event.eor("`Processing...`")
     text = str(event.pattern_match.group(1)).strip()
     if len(text) < 1:
-        return await msg.edit("`Berikan pesan...`")
+        return await msg.eor("`Berikan pesan...`")
     meme = await drawText(file, text)
-    await bot.send_file(event.chat_id, file=meme, force_document=False)
+    await event.client.send_file(event.chat_id, file=meme, force_document=False)
     await msg.delete()
     os.remove(meme)
 
