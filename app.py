be6ab120416181ba1291dfabe1a57f83688955a4
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-08 05:13:12 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import os
-from dotenv import load_dotenv, set_key
+from dotenv import load_dotenv
+from heroku3 import from_key
 
 load_dotenv()
 
-redis_url = os.environ.get("REDISCLOUD_URL", "")
+heroku_api = os.environ.get("HEROKU_API")
+heroku_app_name = os.environ.get("HEROKU_APP_NAME")
+heroku_conn = from_key(heroku_api)
+heroku_app = heroku_conn.apps()[heroku_app_name]
+heroku_config = heroku_app.config()
 
+redis_url = os.environ.get("REDISCLOUD_URL")
 if redis_url.startswith("redis://"):
     redis_url = redis_url.replace("redis://", "", 1)
     redis_uri, redis_password = redis_url.split("@")
     redis_password = redis_password.split(":")[1]
     
-    os.environ["REDIS_URI"] = redis_uri
-    set_key(".env", "REDIS_URI", redis_uri)
-    os.environ["REDIS_PASSWORD"] = redis_password
-    set_key(".env", "REDIS_PASSWORD", redis_password)
+    heroku_config["REDIS_URI"] = redis_uri
+    heroku_config["REDIS_PASSWORD"] = redis_password
 
 else:
     raise ValueError("Invalid REDISCLOUD_URL")
