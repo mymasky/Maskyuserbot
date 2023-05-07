1d148cf12edb532bfa4e62f88f43f927ef5df612
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-08 04:54:45 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import os
+from dotenv import load_dotenv, set_key
+
+load_dotenv()
 
 redis_url = os.environ.get("REDISCLOUD_URL", "")
+
 if redis_url.startswith("redis://"):
     redis_url = redis_url.replace("redis://", "", 1)
     redis_uri, redis_password = redis_url.split("@")
     redis_password = redis_password.split(":")[1]
-
-    # Set REDIS_URI and REDIS_PASSWORD as environment variables
+    
     os.environ["REDIS_URI"] = redis_uri
+    set_key(".env", "REDIS_URI", redis_uri)
     os.environ["REDIS_PASSWORD"] = redis_password
+    set_key(".env", "REDIS_PASSWORD", redis_password)
 
 else:
     raise ValueError("Invalid REDISCLOUD_URL")
