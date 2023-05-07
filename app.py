337f20c008568a4cd7adbbdc6d2131c62c1eabd0
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-08 03:58:49 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
-from urllib.parse import urlparse
 
-from Ayra.startup._database import RedisDB
+redis_url = os.environ.get("REDISCLOUD_URL", "")
+if redis_url.startswith("redis://"):
+    redis_url = redis_url.replace("redis://", "", 1)
+    redis_uri, redis_password = redis_url.split("@")
+    redis_password = redis_password.split(":")[0]
+    host_port = redis_uri.split(":")
+    host = host_port[0]
+    port = int(host_port[1])
 
-redis_url = os.environ.get("REDISCLOUD_URL")
-if redis_url:
-    url = urlparse(redis_url)
-    redis_db = RedisDB(
-        host=url.hostname,
-        port=url.port,
-        password=url.password,
-    )
-    REDIS_URI = f"redis://{url.hostname}:{url.port}"
-    REDIS_PASSWORD = url.password
+    # Set REDIS_URI and REDIS_PASSWORD as environment variables
+    os.environ["REDIS_URI"] = f"redis://{host}:{port}"
+    os.environ["REDIS_PASSWORD"] = redis_password
+
 else:
     raise ValueError("Invalid REDISCLOUD_URL")
