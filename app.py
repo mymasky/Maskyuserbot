97e29ee74f27b71695e2254267606e3a469d7c2f
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-08 04:18:34 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import os
+from redis import Redis
 
 redis_url = os.environ.get("REDISCLOUD_URL", "")
+
 if redis_url.startswith("redis://"):
     redis_url = redis_url.replace("redis://", "", 1)
-    redis_uri, redis_password = redis_url.split("@")
-    redis_password = redis_password.split(":")[0]
-    host_port = redis_uri.split(":")
-    host = host_port[0]
-    port = int(host_port[1])
+    redis = Redis.from_url(redis_url)
+    host, port, password = redis.connection_pool.connection_kwargs.values()
 
-    # Set REDIS_URI and REDIS_PASSWORD as environment variables
     os.environ["REDIS_URI"] = f"redis://{host}:{port}"
-    os.environ["REDIS_PASSWORD"] = redis_password
+    os.environ["REDIS_PASSWORD"] = password
 
 else:
     raise ValueError("Invalid REDISCLOUD_URL")
