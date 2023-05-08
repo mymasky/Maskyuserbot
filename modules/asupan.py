44012f583a4ded8bcfaa62c2ec108a7d3e755b8a
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-08 10:12:29 +0700

    update asupan.py

--- modules/asupan.py
+++ modules/asupan.py
@@ -109,7 +109,7 @@ async def _(event):
             )
         ]
         await event.client.send_file(
-            event.chat_id, file=choice(ppcpnya), reply_to=event.reply_to_msg_id
+            event.chat_id, file=choice(ppcpnya), reply_to=event.reply_to_msg_id, caption=f"**Ppcp Nya {inline_mention(event.sender)}..**",
         )
         await xx.delete()
     except Exception:
@@ -127,7 +127,7 @@ async def _(event):
             )
         ]
         await event.client.send_file(
-            event.chat_id, file=choice(ajgg), reply_to=event.reply_to_msg_id
+            event.chat_id, file=choice(ajgg), reply_to=event.reply_to_msg_id, caption=f"**Ppcp Nya {inline_mention(event.sender)}..**",
         )
         await xx.delete()
     except Exception:
@@ -145,7 +145,7 @@ async def _(event):
             )
         ]
         await event.client.send_file(
-            event.chat_id, file=choice(nimek), reply_to=event.reply_to_msg_id
+            event.chat_id, file=choice(nimek), reply_to=event.reply_to_msg_id, caption=f"**Anime Nya {inline_mention(event.sender)}..**",
         )
         await xx.delete()
     except Exception:
@@ -163,7 +163,7 @@ async def _(event):
             )
         ]
         await event.client.send_file(
-            event.chat_id, file=choice(nimekk), reply_to=event.reply_to_msg_id
+            event.chat_id, file=choice(nimekk), reply_to=event.reply_to_msg_id, caption=f"**Anime Nya {inline_mention(event.sender)}..**",
         )
         await xx.delete()
     except Exception:
@@ -181,7 +181,7 @@ async def _(event):
             )
         ]
         await event.client.send_file(
-            event.chat_id, file=choice(papjing), reply_to=event.reply_to_msg_id
+            event.chat_id, file=choice(papjing), reply_to=event.reply_to_msg_id, caption=f"**Pap Nya {inline_mention(event.sender)}..**",
         )
         await xx.delete()
     except Exception:
