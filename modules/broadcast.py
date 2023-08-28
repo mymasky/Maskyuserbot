fa9ab01686f1703e3c3fb68188be98757e0de0ff
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-08-28 17:39:18 +0700

    update broadcast.py

--- modules/broadcast.py
+++ modules/broadcast.py
@@ -61,8 +61,7 @@ async def gcast(event):
                 )
             ):
                 try:
-                  await event.client.send_message(
-                    chat, msg)
+                    await event.client.send_message(chat, msg)
                     done += 1
                 except FloodWaitError as fw:
                     await asyncio.sleep(fw.seconds + 10)
