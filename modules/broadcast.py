84b4eb5bb6babe67b179944ae46c8a6908ea49ef
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-09-13 17:41:53 +0700

    update broadcast.py

--- modules/broadcast.py
+++ modules/broadcast.py
@@ -47,14 +47,13 @@ async def gcast(event):
     er = 0
     done = 0
     err = ""
-    chat_blacklist = udB.get_key("GBLACKLISTS") or []
-    chat_blacklist.append(-1001812143750)
-    udB.set_key("GBLACKLISTS", chat_blacklist)
+    chat_blacklist = udB.get_key("GBLACKLISTS")
+    #chat_blacklist.append(-1001812143750)
+    #udB.set_key("GBLACKLISTS", chat_blacklist)
     async for x in event.client.iter_dialogs():
         if x.is_group:
             chat = x.id
             
-
             if chat not in chat_blacklist and chat not in NOSPAM_CHAT:
                 try:
                     await event.client.send_message(chat, msg)
