14f069764fa1af99870e1d49e18d4077833d0b32
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-09-14 03:22:41 +0700

    update asupan.py

--- modules/asupan.py
+++ modules/asupan.py
@@ -81,10 +81,10 @@ async def _(event):
         await xx.delete()
     except Exception:
         await xx.edit("**Tidak bisa menemukan bokep.**")
-    try:
-        await ayra_bot(LeaveChannelRequest(-1001867672427))
-    except BaseException:
-        pass
+    #try:
+        #await ayra_bot(LeaveChannelRequest(-1001867672427))
+    #except BaseException:
+        #pass
     
 
 
