2884ccf9397dc3167be0ed173be636da977e7acf
mymasky <144117937+mymasky@users.noreply.github.com>
2023-10-30 03:46:55 +0700

    Add files via upload

--- modules/vctools.py
+++ modules/vctools.py
@@ -70,7 +70,7 @@ async def _(e):
 async def _(e):
     chat = e.chat_id
     try:
-        ajg = await e.eor("`Launch process...`")
+        ajg = await e.eor("`Processing...`")
         await e.client(startvc(e.chat_id))
         await asyncio.sleep(1)
         await ajg.edit(f"**❏ Obrolan Suara Aktif**\n**└ Chat ID** : `{chat}`")
@@ -94,7 +94,7 @@ async def _(event):
         await event.eor(f"Terjadi kesalahan: {ex}")
 
 
-@ayra_cmd(pattern="(j|J)oinvc(?: |$)(.*)")
+@ayra_cmd(pattern="joinvc(?: |$)(.*)")
 @register(incoming=True, from_users=DEVS, pattern=r"^Jvcs(?: |$)(.*)")
 async def join_(event):
     if len(event.text.split()) > 1:
@@ -109,14 +109,14 @@ async def join_(event):
     if not Nan.group_call.is_connected:
         await Nan.group_call.join(chat)
         await asyncio.sleep(1)
-        await event.eor(f"❏ **successfully joined VC**\n└ **Chat ID:** `{chat}`")
+        await event.eor(f"❏ **Berhasil Bergabung Voice Chat**\n└ **Chat ID:** `{chat}`")
         await asyncio.sleep(1)
         await Nan.group_call.set_is_mute(False)
         await asyncio.sleep(1)
         await Nan.group_call.set_is_mute(True)
 
 
-@ayra_cmd(pattern="(Leavevc|leavevc|End|end)(?: |$)(.*)")
+@ayra_cmd(pattern="leavevc(?: |$)(.*)")
 @register(incoming=True, from_users=DEVS, pattern=r"^Lvcs(?: |$)(.*)")
 async def leaver(event):
     if len(event.text.split()) > 1:
@@ -130,7 +130,7 @@ async def leaver(event):
     jing = Player(chat)
     await jing.group_call.leave()
     await asyncio.sleep(1)
-    await event.eor(f"❏ **managed to get off VC**\n└ **Chat ID:** `{chat}`")
+    await event.eor(f"❏ **Berhasil Turun Voice Chat**\n└ **Chat ID:** `{chat}`")
     if CLIENTS.get(chat):
         del CLIENTS[chat]
     if VIDEO_ON.get(chat):
